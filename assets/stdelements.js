c2855f5c82f8bb9351301d9f84faec34554aaa4b
Vrabbers <nininininimocp@gmail.com>
2018-12-27 23:31:06 -0200

    standard elements, shavian page start

---
+++ assets/stdelements.js
@@ -0,0 +1,12 @@
+function navButtons(){
+    document.write(`
+    <button onclick="window.location.href='index.html'">Home</button>
+    <button onclick="window.location.href='shavian.html'">Shavian</button>`)
+}
+function footer(){
+    document.write(` 
+    Made by <a href="https://github.com/Vrabbers">Vrabbers</a>
+    <p>
+    Mostly an experimental sandbox.<p>
+    Blob emoji by <a href="https://github.com/C1710/blobmoji">C1710</a>`)
+}
