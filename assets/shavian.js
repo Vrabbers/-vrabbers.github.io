3869c05fa9efeb7652238400782be6caf16c3aea
Vrabbers <nininininimocp@gmail.com>
2019-01-05 18:24:11 -0200

    i am bad at coding

--- assets/shavian.js
+++ assets/shavian.js
@@ -1,3 +1,12 @@
-$(document).ready(()=>{
+var strings;
 
+$.getJSON("https://vrabbers.github.io/assets/shawstrings.json", (json)=>{
+    strings=json;
+});
+
+
+$(document).ready(()=>{
+    while(typeof strings == 'undefined'){
+    }
+    console.log(strings)
 });
