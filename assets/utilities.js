3488e8001566145479fbdcceb0fa9a4bb1daa45d
Vrabbers <nininininimocp@gmail.com>
2019-01-13 16:17:37 -0200

    got rid of the footer blegh anyway

--- assets/utilities.js
+++ assets/utilities.js
@@ -1,11 +1,6 @@
 function navButtons(){
     document.write(`
     <button onclick="window.location.href='index.html'">Home</button>
-    <button onclick="window.location.href='shavian.html'">Shavian</button>`)
-}
-function footer(){
-    document.write(` 
-    Made by <a href="https://github.com/Vrabbers">Vrabbers</a><p>
-    Mostly an experimental sandbox.<p>
-    Blob emoji by <a href="https://github.com/C1710/blobmoji">C1710</a>`)
+    <button onclick="window.location.href='shavian.html'">Shavian</button>
+    <button onclick="window.location.href='info.html'">About</button>`)
 }
